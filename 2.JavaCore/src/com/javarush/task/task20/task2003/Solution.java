e692c3927889cdb70fd35351c903f63d9a38c02b
zoom59rus <anton_1@bk.ru>
2019-07-03 21:54:28 +0500

    2005

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task20.task2003;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* 
 Знакомство с properties
@@ -8,16 +11,23 @@ import java.io.*;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
-        //implement this method - реализуйте этот метод
+    public void fillInPropertiesMap() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        InputStream inputStream = new FileInputStream(reader.readLine());
+        load(inputStream);
     }
 
     public void save(OutputStream outputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties propertie = new Properties();
+        propertie.putAll(properties);
+        propertie.store(outputStream, null);
+        outputStream.flush();
     }
 
     public void load(InputStream inputStream) throws Exception {
-        //implement this method - реализуйте этот метод
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        properties.forEach((k, v) -> this.properties.put(k.toString(), v.toString()));
     }
 
     public static void main(String[] args) {
